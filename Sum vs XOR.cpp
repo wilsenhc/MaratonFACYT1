c2e956f9269ad8d3daf7d8d0b9e9f8faa6482477
Wilsen Hernandez <wilsenh95@gmail.com>
2017-12-21 20:41:25 -0400

    Submission 2

--- Sum vs XOR.cpp
+++ Sum vs XOR.cpp
@@ -7,13 +7,28 @@ int main() {
     long result = 0;
     long ab = 0;
     cin >> n;
-    m = n;
-    for (long i = 0; i <= n; i++, m++)
+    if (n > 100)
     {
-        ab = n^i;
-        if (m == ab)
+        while (n != 0)
+        {
+            if ((n & 1) == 0)
+            {
                 result++;
+            }
+            n = n >> 1;
+        }
+        cout << pow(2, result) << endl;
     }
-    cout << result << endl;
+    else
+    {
+        m = n;
+        for (long i = 0; i <= n; i++, m++)
+        {
+            ab = n^i;
+            if (m == ab)
+                result++;
+        }
+        cout << result << endl;
+    }
     return 0;
 }
